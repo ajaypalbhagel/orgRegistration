b6f0e5266411eb6df0e4634524ff229cb0e3345c
ajay <ajaypalbhagel@gmail.com>
2021-02-16 12:37:28 +0530

    s

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
-const port =  process.env.port ;
+const port =  process.env.port || 3000;
 const http = require('http')
 const app = require('./app'); 
 const server = http.createServer(app);
-
+console.log(port)
 server.listen(port);
 
