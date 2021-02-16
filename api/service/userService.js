fb5f6dd95fcbfed9e1a8e8e54bcdbb9a54dbab08
ajay <ajaypalbhagel@gmail.com>
2021-02-16 14:01:48 +0530

    asdfdsf

--- api/service/userService.js
+++ api/service/userService.js
@@ -7,7 +7,6 @@ var randtoken = require('rand-token')
 const userLoginSession = require("../model/userLoginSession")
 const tokenExpireTime = { expiresIn: "30m" }
 const tokenSecretKey = 'secret!@#$!@!@'
-const uuidv1 = require('uuid/v1');
 
 // user signup api service
 
@@ -47,7 +46,7 @@ async function userLogin(request) {
     let error = false
     var responseData = {}
     let errorMsg = ''
-    
+    const uuidv1 = require('uuid/v1');
     const serverUniqueId = uuidv1()
     const clientId = request.client_id
 
