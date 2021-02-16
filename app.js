c51783a0c675963b3440b0d00ff9aaf7f04ff5b7
ajay <ajaypalbhagel@gmail.com>
2021-02-16 12:24:34 +0530

    ,

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 // const userRoutes = require('./api/controller/userController')
 var app = express();
 app.use(express.json());
-app.use('/',(req,res)=>{res.send('welcome ajay')})
+app.get('/',(req,res)=>{res.send('welcome ajay')})
 const port = process.env.PORT || '5000';
 app.listen(port,()=>console.log('server',port));
 
